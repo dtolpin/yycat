87c10ef2c7fe987dab4a33eff2a9bfacc4718bde
David Tolpin <david.tolpin@gmail.com>
2012-03-28 17:04:47 +0200

    garbage removed from jeru csv

--- tools/jeru2csv.py
+++ tools/jeru2csv.py
@@ -1,12 +1,20 @@
 import sys
 import re
 import csv
+import HTMLParser
+unescape = HTMLParser.HTMLParser().unescape
 
 prevline = ""
 outp = csv.writer(sys.stdout)
 fields = []
 for line in sys.stdin:
-	line = line.strip()
+	line = line.strip() \
+		.replace("&quot;", "\"") \
+		.replace("&amp;", "&") \
+		.replace("&aacute;", "a") \
+		.replace("&acirc;", "a")
+	
+	
 	if prevline:
 		line = prevline+" "+line
 	if re.search("<tr", line):
