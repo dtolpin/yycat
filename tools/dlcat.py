90b51de26a6bc97eafdd0bbaee2512d21092e9aa
David Tolpin <david.tolpin@gmail.com>
2012-05-22 15:31:37 +0300

    extended dlcat to download any sheet, fixed bug with last line

--- tools/dlcat.py
+++ tools/dlcat.py
@@ -17,32 +17,33 @@ def yycatkey(gd_client):
 			key = entry.id.text.split('/')[-1]
 			return key
 
-def bklistid(gd_client, skey):
+def sheetid(gd_client, skey, sheet):
 	"""retrieves worksheet id for the booklist"""
 	feed = gd_client.GetWorksheetsFeed(skey)
 	for i, entry in enumerate(feed.entry):
-		if entry.title.text=='booklist':
+		if entry.title.text==sheet:
 			wid = entry.id.text.split('/')[-1]
 			return wid
 
-def download(gd_client, outp):
+def download(gd_client, outp, sheet='booklist'):
 	"""writes the book list as a CSV to outp"""
 	skey = yycatkey(gd_client)
-	wid = bklistid(gd_client, skey)
+	wid = sheetid(gd_client, skey, sheet)
 	feed = gd_client.GetCellsFeed(skey, wid)
 	cat = csv.writer(sys.stdout)
 	icol, irow = "A", ""
 	lastcol = "A"
 	row = []
+
 	for i, entry in enumerate(feed.entry):
 		jcol, jrow = entry.title.text[:1], entry.title.text[1:]
 		if irow!=jrow:
 			if lastcol<icol:
 				lastcol = icol
-			while icol<lastcol:
-				row.append("")
-				icol = chr(ord(icol)+1)
 			if row:
+				while icol<lastcol:
+					row.append("")
+					icol = chr(ord(icol)+1)
 				cat.writerow(row)
 				row = []
 			icol, irow = "A", jrow
@@ -51,10 +52,16 @@ def download(gd_client, outp):
 			icol = chr(ord(icol)+1)
 		row.append(entry.content.text)
 		icol = chr(ord(icol)+1)
+	# flush the last line
+	if row:
+		while icol<lastcol:
+			row.append("")
+			icol = chr(ord(icol)+1)
+		cat.writerow(row)
 
 if __name__=="__main__":
 	import sys
 	gd_client = connect()
-	download(gd_client, sys.stdout)
+	download(gd_client, sys.stdout, *sys.argv[1:])
 
 
